9c605e40b3e2c7fbbc40f4e5eba6759191246899
upluke <lukeforadream@gmail.com>
2021-06-13 23:27:09 -0700

    set app state

--- src/App.js
+++ src/App.js
@@ -1,10 +1,43 @@
- 
+import React, {useEffect, useState} from 'react'
 import './App.css';
+import List from './components/List'
+import WithListLoading from './components/withListLoading'
 
 function App() {
+  const ListLoading =WithListLoading(List)
+  const [appState, setAppState]=useState({
+    loading:false,
+    repos:null,
+  })
+
+  useEffect(() => {
+    setAppState({loading:true})
+    const apiUrl=`https://api.github.com/users/hacktivist123/repos`;
+    fetch(apiUrl)
+      .then((res)=>res.json())
+      .then((repos)=>{
+      setAppState({loading:false, repos:repos})
+    })
+ 
+  }, [setAppState])
+
+
   return (
     <div className="App">
-        <h1>My REST</h1>
+        <div className='container'>
+            <h1>My repos</h1>
+        </div>
+        <div className='repo-container'>
+          <ListLoading isLoading={appState.loading} repos={appState.repos} />
+        </div>
+        <footer>
+          <div>
+            Built{' '}
+            <span role='img' aria-label='love'>
+                💚
+            </span>{' '}
+          </div>
+        </footer>
     </div>
   );
 }
